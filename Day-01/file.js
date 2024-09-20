15082d29bae1acc773eaaa383443c7827e137dfb
krishigandhi <98482670+krishigandhi@users.noreply.github.com>
2024-09-20 05:52:43 +0000

    Classes

---
+++ Day-01/file.js
@@ -0,0 +1,33 @@
+// Activity -1 
+
+var a = 12;
+console.log(a);
+console.log(typeof(a));
+
+let b = "hello";
+console.log(b);
+console.log(typeof(b));
+
+// Activity -2
+const d = true;
+console.log(d);
+console.log(typeof(d));
+
+// Activity -3
+let e = {
+    a:"name",
+    b:"Add"
+}
+console.log(typeof(e));
+
+let f = [1,2,3,4];
+console.log(Array.isArray(f));
+
+// Activity -4
+ f = "Hii";
+ console.log(f)
+
+// Activity -5
+ d = false;
+console.log(d);
+
