85e6ceef7462f15e76b9e3d82d95b253e563c5a7
Krishi Gandhi <krishigandhi2601@gmail.com>
2024-09-21 14:15:48 +0000

    Closure

--- Day-15/file.js
+++ Day-15/file.js
@@ -1,7 +1,14 @@
 // Activity - 1
-function one(params) {
-    
+function add(a,b) {
+     a = 4;
+     b= 3;
+    function sum(a,b){
+         return a + b;
+    }
+   return sum (a,b) ;
 }
+console.log(`Additon: ${add()}`)
+// console.log(sum)
 // Activity - 2
 // Activity - 3
 // Activity - 4
